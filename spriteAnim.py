095b9cb211c6632dd93383ed352fb1289562ceae
Tsvetan Tsvetanov <gogo_rus@abv.bg>
2013-07-14 17:19:54 +0300

    added sprite anim

--- spriteAnim.py
+++ spriteAnim.py
@@ -28,3 +28,37 @@ class Spritesheet(object):
 		tups = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
 				for x in range(imageCount)]
 		return self.loadListOfImagesAt(tups, colorKey)
+
+
+class SpriteStripAnim(object):
+	def __init__(self, fileName, rect, count,\
+			     colorKey = None, loop = False, frames = 1):
+		self.fileName = fileName
+		ss = Spritesheet(fileName)
+		self.images = ss.loadImageStrip(rect, count, colorKey)
+		self.i = 0
+		self.loop = loop
+		self.frames = frames
+		sefl.f = frames
+
+	def iter(self):
+		self.i = 0
+		self.f = self.frames
+		return self
+
+	def next(self):
+		if self.i >= len(self.images):
+			if not self.loop:
+				raise StopIteration
+			else:
+				self.i = 0
+		image = self.images[self.i]
+		self.f -= 1
+		if self.f == 0:
+			self.i += 1
+			self.f = self.frames
+		return image	
+	
+	def __add__(self, ss):
+		self.images.extend(ss.images)
+		return self
